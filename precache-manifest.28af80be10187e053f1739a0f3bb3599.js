1470a839ab862386705309951819f69b1f88cb2a
Tran Van Tai <tai.tran@pn.com.vn>
2019-10-28 15:35:30 +0700

    Initial Commit

---
+++ precache-manifest.28af80be10187e053f1739a0f3bb3599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e8f7ed8716e9368b9146ea50fe2ffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a3da3fc5228579468ee",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "565c8cdf7a6439c2aab9",
+    "url": "/static/css/main.a12dcc57.chunk.css"
+  },
+  {
+    "revision": "6a3da3fc5228579468ee",
+    "url": "/static/js/2.a1d4eb07.chunk.js"
+  },
+  {
+    "revision": "565c8cdf7a6439c2aab9",
+    "url": "/static/js/main.0965954b.chunk.js"
+  },
+  {
+    "revision": "8753f0d8bcf9b0313506",
+    "url": "/static/js/runtime-main.a7983923.js"
+  }
+]);
